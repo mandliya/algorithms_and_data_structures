3567efac9f079579e2aeddafd1aa4d3dbe0a57e1
mandliya <ravi.mandliya@gmail.com>
2014-08-06 11:06:26 -0400

    Day1-Reverse the words of a sentence

---
+++ day1-reverseSentence.cpp
@@ -0,0 +1,10 @@
+/*	day 1 - August 8 2014
+	Problem : Reverse words in a sentence.
+	example Input : The food tastes awesome
+	example Output: awesome tastes food The
+
+	Algorithm:
+		Reverse the entire sentence; the current sentence becomes - 'emosewa setsat doof ehT'
+		Reverse the individual words now, the sentence becomes    - 'awesome tastes food The'
+
+*/
