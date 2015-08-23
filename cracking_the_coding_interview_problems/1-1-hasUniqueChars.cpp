ff635aaf139e4d8569bcaeb62630747679424f62
mandliya <ravi.mandliya@gmail.com>
2015-08-23 16:45:55 -0400

    Day-7- cracking the coding interview 1.1

---
+++ cracking_the_coding_interview_problems/1-1-hasUniqueChars.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <cstring>
+#include <vector>
+
+/*
+ * Function hasUniqueChars1
+ * Params - std::string
+ * Output:: True if string has all characters which are unique
+ *          False if string has at least one repeated char.
+ * Assumption:: ASCII chars, 256 chars.
+ */
+bool hasUniqueChars1( std::string str)
+{
+    int len = str.length();
+    int check[8]; //represents 256 bits.
+    std::memset( check, 0 , sizeof check );
+    int idx, v, shift;
+
+    for ( int i = 0; i < len; ++i) {
+        v = (int) str[i];
+        idx = v / 32;      //identify which int bucket will represent this char
+        shift = v % 32;    //identify which bit in above int will represent the char 
+        if ( check[idx] & ( 1 << shift ) ) {
+            return false;
+        }
+        check[idx] |= ( 1 << shift );
+    }
+    return true;
+}
+
+/*
+ * Function hasUniqueChars2
+ * Params - std::string
+ * Output:: True if string has all characters which are unique
+ *          False if string has at least one repeated char.
+ * Assumption:: string only contains (a..z), 26 chars.
+ */
+
+bool hasUniqueChars2( std::string str)
+{
+    int check = 0;
+    int len = str.length();
+    for (int i = 0; i < len; ++i)
+    {
+        int c = (int)(str[i] - 'a');
+        if ( check & ( 1 << c ) ) {
+            return false;
+        }
+        check |= ( 1 << c);
+    }
+    return true;
+}
+
+
+int main ()
+{
+    std::string word1("copyrightable");
+    std::string word2("Dermatoglyphics!");
+    std::string word3("abiogenesis");
+    std::string word4("Centrobaric!");
+
+    //a word with unique chars (a-z)
+    std::cout << "Does " << word1 << " has unique chars :" << hasUniqueChars2(word1) << std::endl;
+
+    //a word with unique ASCII chars
+    std::cout << "Does " << word2 << " has unique chars :" << hasUniqueChars1(word2) << std::endl;
+
+    //a word with repeated chars (a-z)
+    std::cout << "Does " << word3 << " has unique chars :" << hasUniqueChars2(word3) << std::endl;
+    
+    //a word with repeated AsCII chars
+    std::cout << "Does " << word4 << " has unique chars :" << hasUniqueChars1(word4) << std::endl;
+    return 0;
+
+}
+
+
