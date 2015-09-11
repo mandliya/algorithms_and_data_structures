1349f21a01516dd1bb1db078e2030041b799e607
Ravi Mandliya <ravi.mandliya@gmail.com>
2015-09-10 21:50:25 -0400

    Update swapNodesWithoutSwappingData.cpp

--- linked_list_problems/swapNodesWithoutSwappingData.cpp
+++ linked_list_problems/swapNodesWithoutSwappingData.cpp
@@ -1,6 +1,7 @@
 /*
  * Problem : Given a linked list, and two data values, x and y. 
- * Goal:     Swap the nodes of linkedlist by swapping pointers ( and not swapping data) such that nodes with data x will contain y and vice versa.
+ * Goal:     Swap the nodes of linkedlist by swapping pointers 
+ *           (and not swapping data) such that nodes with data x will contain y and vice versa.
  *
  */
 
