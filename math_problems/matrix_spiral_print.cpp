9b59967a273e7ed005df8f8af76f207bb4b5a786
Ravi Mandliya <ravi.mandliya@gmail.com>
2015-09-27 00:24:09 -0400

    Day 42 spiral print of matrix

--- math_problems/matrix_spiral_print.cpp
+++ math_problems/matrix_spiral_print.cpp
@@ -1,3 +1,14 @@
+/**
+ *  Given a m * n matrix, print the contents of it spiral way. for example.
+ *  Matrix is :
+ *      1  2  3  4  5 
+ *      6  7  8  9  10
+ *      11 12 13 14 15
+ *      16 17 18 19 20
+ *  Sprial order print would
+ *  1 2 3 4 5 10 15 20 19 18 17 16 11 6 7 8 9 14 13 12
+ * /
+
 #include <iostream>
 
 void spiral_print_matrix( int ** matrix, int m, int n)
