2cd77828f1cb9194fa1c887fa01ec575631788f1
Ravi Mandliya <ravi.mandliya@gmail.com>
2015-10-16 00:21:58 -0400

    Day 61 update catalan comments

--- dynamic_programming_problems/catalan_number.cpp
+++ dynamic_programming_problems/catalan_number.cpp
@@ -4,17 +4,17 @@
  *  n = 1 , num of BST - 1
  *  n = 2 , num of BST - 2
  *
- *  						2                 1
- *        		/       and           \
+ *              2                 1
+ *            /       and           \
  *          1                       2
  *
  *  n = 3, num of BST - 5
  *
- *  					3          1               2             2                 3
- *  				/             \             / \             \               /
- *  			 2       and     2     and   1   3   and       3     and     1
- *  			/                \                            /               \
- *  		 1                  3                          1                2
+ *            3          1               2             2                 3
+ *          /             \             / \             \               /
+ *        2       and     2     and   1   3   and       3     and     1
+ *        /                \                            /               \
+ *      1                  3                          1                2
  *
  * So, If we choose, ith element as root, there will be i-1 elements on the
  * left subtree and n-i on the right. The two side subtrees would be independent
