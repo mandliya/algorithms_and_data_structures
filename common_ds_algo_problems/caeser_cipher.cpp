eec5f4878c5b1b547ac6f28d6ca43351c05063f1
Ravi Mandliya <ramand@microsoft.com>
2017-04-09 13:34:58 -0700

    Updated caeser_cipher

---
+++ common_ds_algo_problems/caeser_cipher.cpp
@@ -0,0 +1,82 @@
+/*
+ * Encrypts a plain text to decrypts an encrypted text back to plain using Caeser Cipher.
+ * Example:
+ * Enter plain text:the brown fox jumped over the lazy dog
+ * Enter shift length:24
+ * Encrypted text:rfc zpmul dmv hskncb mtcp rfc jyxw bme
+ * Decrypted text:the brown fox jumped over the lazy dog
+ */
+
+#include <iostream>
+#include <string>
+#include <sstream>
+
+char caeser_encrypt(char c, int shift_length);
+char caeser_decrypt(char c, int shift_length);
+
+int main()
+{
+    std::string plain_text, encrypted_text;
+    int shift_length;
+    std::cout << "Enter plain text:";
+    std::getline(std::cin, plain_text);
+    std::cout << "Enter shift length:";
+    std::cin >> shift_length;
+    std::stringstream ss;
+
+    unsigned int text_length = plain_text.length();
+
+    // Encrypt the text
+    //
+    for (unsigned int i = 0; i < text_length; ++i)
+    {
+        ss << caeser_encrypt(plain_text[i], shift_length);
+    }
+
+    encrypted_text = ss.str();
+    std::cout << "Encrypted text:" << encrypted_text << std::endl;
+
+    // Reset the stream
+    //
+    ss.str(std::string());
+    
+    // Decrypt the text again
+    //
+    for (unsigned int i = 0; i < text_length; ++i)
+    {
+        ss << caeser_decrypt(encrypted_text[i], shift_length);
+    }
+
+    std::cout << "Decrypted text:" << ss.str() << std::endl;
+    return 0;
+}
+
+char caeser_encrypt(char c, int shift_length)
+{
+    if (c >= 'A' && c <= 'Z')
+    {
+        return (((c - 'A' + shift_length) % 26) + 'A');
+    }
+    else if (c >= 'a' && c <= 'z')
+    {
+        return (((c - 'a' + shift_length) % 26) + 'a');
+    }
+    
+    // In case of non-alpha characters, return as it is.
+    return c;
+}
+
+char caeser_decrypt(char c, int shift_length)
+{
+    if (c >= 'A' && c <= 'Z')
+    {
+        return (((c - 'A' + (26 - shift_length)) % 26) + 'A');
+    }
+    else if (c >= 'a' && c <= 'z')
+    {
+        return (((c - 'a' + (26 - shift_length)) % 26) + 'a');
+    }
+    
+    // In case of non-alpha characters, return as it is.
+    return c;
+}
