36391f7c4abc9064fab087c21f2acdc33b911950
pradnya809 <68142549+pradnya809@users.noreply.github.com>
2020-10-01 23:23:02 +0530

    Update bubbleSortDemo.cpp

--- sort_search_problems/bubbleSortDemo.cpp
+++ sort_search_problems/bubbleSortDemo.cpp
@@ -18,40 +18,39 @@
  */
 
 
-#include <bubbleSort.h>	//do not assume people will have their path set
-#include <generic.h>
-#include <stdlib.h>
-#include <time.h>
-
-int main()
-{
-    const int MAX_ELEMENTS = 10;
-    int arr[MAX_ELEMENTS];
-    double arrD[MAX_ELEMENTS];
-	srand(time(0));
-	
-    //Filling up the array with random numbers;
-    for (int i = 0; i < MAX_ELEMENTS; ++i)
-    {   
-        // arr[i] = algo::random_range(1, 100);	//does not generate random number everytime
-        //arrD[i] = algo::random_range(1.0, 99.99);	//does not generate random
-		arr[i] = rand() % 100 + 1;
-		double f = (double)rand() / RAND_MAX;
-		arrD[i] = 1.0 + f * (99.99 - 1.0);
+#include < iostream >
+  using namespace std;
+ 
+void bubbleSort(int a[]) {
+  for (int i = 0; i < 5; i++) {
+    for (int j = 0; j < (5 - i - 1); j++) {
+      if (a[j] > a[j + 1]) {
+        int temp = a[j];
+        a[j] = a[j + 1];
+        a[j + 1] = temp;
+      }
     }
-
-    std::cout << "Before Sorting:\n";
-    algo::printList(arr, MAX_ELEMENTS);
-    algo::bubbleSort(arr, 0, MAX_ELEMENTS - 1);
-    std::cout << "After Sorting:\n";
-    algo::printList(arr, MAX_ELEMENTS);
-
-
-    std::cout << "\n\nBefore Sorting:\n";
-    algo::printList(arrD, MAX_ELEMENTS);
-    algo::bubbleSort(arrD, 0, MAX_ELEMENTS - 1);
-    std::cout << "After Sorting:\n";
-    algo::printList(arrD, MAX_ELEMENTS);
-    return 0;
+  }
 }
-
+ 
+int main() {
+  int myarray[5];
+  int size;
+  cout << "Enter 5 integers in any order: " << endl;
+  for (int i = 0; i < 5; i++) {
+    cin >> myarray[i];
+  }
+  cout << "Before Sorting" << endl;
+  for (int i = 0; i < 5; i++) {
+    cout << myarray[i] << " ";
+  }
+ 
+  bubbleSort(myarray); // sorting
+ 
+  cout << endl << "After Sorting" << endl;
+  for (int i = 0; i < 5; i++) {
+    cout << myarray[i] << " ";
+  }
+ 
+  return 0;
+}
