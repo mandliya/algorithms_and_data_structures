77f1c6b4d349a66da535e362005dd788e184062b
pradnya809 <68142549+pradnya809@users.noreply.github.com>
2020-10-01 23:26:43 +0530

    Update quickSortDemo.cpp

--- sort_search_problems/quickSortDemo.cpp
+++ sort_search_problems/quickSortDemo.cpp
@@ -17,34 +17,69 @@
  *     Yeah! Ravi let's do it! 
  */
 
-#include <generic.h>
-#include <quickSort.h>
-
+# include <iostream>
+using namespace std;
+// quick sort sorting algorithm
+int Partition(int arr[], int s, int e)
+{
+ int pivot = arr[e];
+ int pIndex = s;
+ 
+ for(int i = s;i<e;i++)
+ {
+ if(arr[i]<pivot)
+ {
+ int temp = arr[i];
+ arr[i] = arr[pIndex];
+ arr[pIndex] = temp;
+ pIndex++;
+ }
+ }
+ 
+ int temp = arr[e];
+ arr[e] = arr[pIndex];
+ arr[pIndex] = temp;
+ 
+ return pIndex;
+}
+ 
+void QuickSort(int arr[], int s, int e)
+{
+ if(s<e)
+ {
+ int p = Partition(arr,s, e);
+ QuickSort(arr, s, (p-1));  // recursive QS call for left partition
+ QuickSort(arr, (p+1), e);  // recursive QS call for right partition
+ }
+}
+ 
 int main()
 {
-    const int MAX_ELEMENTS = 10;
-    int arr[MAX_ELEMENTS];
-    double arrD[MAX_ELEMENTS];
-
-    //Filling up the array with random numbers;
-    for (int i = 0; i < MAX_ELEMENTS; ++i)
-    {   
-        arr[i] = algo::random_range(1, 100);
-        arrD[i] = algo::random_range(1.0, 99.99);
-    }
-
-    std::cout << "Before Sorting:\n";
-    algo::printList(arr, MAX_ELEMENTS);
-    algo::quickSort(arr, 0, MAX_ELEMENTS - 1);
-    std::cout << "After Sorting:\n";
-    algo::printList(arr, MAX_ELEMENTS);
-
-
-    std::cout << "\n\nBefore Sorting:\n";
-    algo::printList(arrD, MAX_ELEMENTS);
-    algo::quickSort(arrD, 0, MAX_ELEMENTS - 1);
-    std::cout << "After Sorting:\n";
-    algo::printList(arrD, MAX_ELEMENTS);
-    return 0;
+ 
+ int size=0;
+ cout<<"Enter Size of array: "<<endl;
+ cin>>size;
+ int myarray[size];
+ 
+ cout<<"Enter "<<size<<" integers in any order: "<<endl;
+ for(int i=0;i<size;i++)
+ {
+ cin>>myarray[i];
+ }
+ cout<<"Before Sorting"<<endl;
+ for(int i=0;i<size;i++)
+ {
+ cout<<myarray[i]<<" ";
+ }
+ cout<<endl;
+ 
+ QuickSort(myarray,0,(size-1));  // quick sort called
+ 
+ cout<<"After Sorting"<<endl;
+ for(int i=0;i<size;i++)
+ {
+ cout<<myarray[i]<<" ";
+ }
+ 
+ return 0;
 }
-
