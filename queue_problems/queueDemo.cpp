cd38bc780cc8196605a40014f8744552a3d14908
me-As-NRB <69763712+me-As-NRB@users.noreply.github.com>
2020-10-02 18:54:43 +0530

    Update queueDemo.cpp

--- queue_problems/queueDemo.cpp
+++ queue_problems/queueDemo.cpp
@@ -1,27 +1,27 @@
 #include <iostream>
 #include "../include/queue.h"
 #include "../include/generic.h"
-
+using namespace std;
 
 int main()
 {
     const int QUEUE_SIZE = 10;
     algo::Queue<int> Q(QUEUE_SIZE);
 
-    std::cout << "Pushing following values to queue:\n";
+    cout << "Pushing following values to queue:\n";
     for ( int i = 0; i < QUEUE_SIZE; ++i )
     {
         int rand_value = algo::random_range( 5, 50 );
-		std::cout << rand_value << " ";
+		cout << rand_value << " ";
         Q.push(rand_value);
     }
-    std::cout << std::endl;
-    std::cout << "Size of Queue is :" << Q.count() << std::endl;
-    std::cout << "\nPopping queue values :\n";
+    cout << endl;
+    cout << "Size of Queue is :" << Q.count() << endl;
+    cout << "\nPopping queue values :\n";
     while ( !Q.empty() ) {
-        std::cout << Q.front() << " ";
+        cout << Q.front() << " ";
         Q.pop();
     }
-    std::cout << std::endl;
+    cout << std::endl;
     return 0;
 }
