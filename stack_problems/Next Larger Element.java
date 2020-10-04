087d53011b8074ed8449b6e7a45d61a846805656
Soumyajit Chakraborty <51930458+Isoumyajit@users.noreply.github.com>
2020-10-04 09:59:57 +0530

    Update Next Larger Element.java

--- stack_problems/Next Larger Element.java
+++ stack_problems/Next Larger Element.java
@@ -1,5 +1,20 @@
 
+/*
+Given an array, print the Next Greater Element (NGE) for every element. The Next greater Element for an element x is the first greater element on the right side of x in array. Elements for which no greater element exist, consider next greater element as -1.
 
+Examples:
+
+For any array, rightmost element always has next greater element as -1.
+For an array which is sorted in decreasing order, all elements have next greater element as -1.
+For the input array [4, 5, 2, 25}, the next greater elements for each element are as follows.
+Element       NGE
+   4      -->   5
+   5      -->   25
+   2      -->   25
+   25     -->   -1
+
+
+*/
 import java.util.*;
 import java.lang.*;
 import java.io.*;
